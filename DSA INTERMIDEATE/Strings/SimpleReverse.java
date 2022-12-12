60b2feffe12d6c4438b802f38294e77461cc6b3a
RAJESH <87864801+rajesh26062@users.noreply.github.com>
2022-12-12 23:18:58 +0530

    Create SimpleReverse.java

---
+++ DSA INTERMIDEATE/Strings/SimpleReverse.java
@@ -0,0 +1,68 @@
+/*Problem Description
+Given a string A, you are asked to reverse the string and return the reversed string.
+
+
+
+Problem Constraints
+1 <= |A| <= 105
+
+String A consist only of lowercase characters.
+
+
+
+Input Format
+First and only argument is a string A.
+
+
+
+Output Format
+Return a string denoting the reversed string.
+
+
+
+Example Input
+Input 1:
+
+ A = "scaler"
+Input 2:
+
+ A = "academy"
+
+
+Example Output
+Output 1:
+
+ "relacs"
+Output 2:
+
+ "ymedaca"
+
+
+Example Explanation
+Explanation 1:
+
+ Reverse the given string. */
+package String;
+
+public class SimpleReverse {
+    public static void main(String[] args) {
+        String  A = "scaler";
+        String output = solve(A);
+        System.out.println(output);
+    }
+    public static String solve(String A) {
+        // converting string to char array
+        char[] charArr = A.toCharArray();
+
+        // swapping characters in array to reverse it
+        for(int i =0,j=charArr.length-1;i<charArr.length/2;i++,j--){
+            char temp  = charArr[i];
+            charArr[i] = charArr[j];
+            charArr[j] = temp;
+        }
+
+        //converting Char array to String
+        String string = new String(charArr);
+        return string; 
+    }
+}
